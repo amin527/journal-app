330e570009ed08f9438ee79bcac6c5403c5730af
amin527 <maparvareshnia@gmail.com>
2025-10-27 13:18:43 +0000

    added the frontend URL as an allowed CORS origin

--- backend/src/main/java/com/example/backend/configurations/SecurityChainConfig.java
+++ backend/src/main/java/com/example/backend/configurations/SecurityChainConfig.java
@@ -55,7 +55,7 @@ public class SecurityChainConfig {
         @Bean
         public CorsConfigurationSource corsConfigurationSource() {
                 CorsConfiguration configuration = new CorsConfiguration();
-                configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+                configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://journal-app-frontend-q7lf.vercel.app"));
                 configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                 configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
                 configuration.setExposedHeaders(List.of("Authorization"));
